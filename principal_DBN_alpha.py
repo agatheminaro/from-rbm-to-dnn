c176af60aa13bcfae20645c53cebd96c68b9b771
agatheminaro <agathe.minaro@ensae.fr>
2023-03-16 11:11:46 +0100

    chgmt

--- principal_DBN_alpha.py
+++ principal_DBN_alpha.py
@@ -2,80 +2,79 @@ from principal_RBM_alpha import RBM
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class DBN:
     def __init__(self, config):
         self.epoch_dbn = []
         self.X_rec_list = []
         self.X_list = []
         self.errors = []
-        
-        
+
         self.RBM_list = []
         self.config = config
-    
+
         for i in range(len(self.config) - 1):
-            self.RBM_list.append(RBM(config[i], config[i+1]))
-    
+            self.RBM_list.append(RBM(config[i], config[i + 1]))
+
     def train_DBN(self, X, epsilon, batch_size, nb_epochs):
         self.epoch_dbn = []
         self.X_rec_list = []
         self.X_list = []
         self.errors = []
         errors_all = []
-            
+
         for epoch in range(nb_epochs):
             X_copy = X.copy()
             for i in range(len(self.RBM_list)):
                 self.RBM_list[i].train_RBM(X_copy, epsilon, batch_size, 1)
                 X_copy = self.RBM_list[i].entree_sortie_RBM(X_copy)
-                
+
             X_rec = X_copy
             for i in reversed(range(len(self.RBM_list))):
                 X_rec = self.RBM_list[i].sortie_entree_RBM(X_rec)
-            
+
             errors_all.append(np.sum((X - X_rec) ** 2) / X.shape[0])
-            
-            if (epoch % 20 == 0):
+
+            if epoch % 20 == 0:
                 rand_idx = np.random.randint(X.shape[0])
-                #if (X[rand_idx].shape[0]==320):
-                self.epoch_dbn.append(f'Epoch{epoch}/{nb_epochs}')
-                self.errors.append(np.sum((X[rand_idx] - X_rec[rand_idx]) ** 2) / X[rand_idx].shape[0])
+                # if (X[rand_idx].shape[0]==320):
+                self.epoch_dbn.append(f"Epoch{epoch}/{nb_epochs}")
+                self.errors.append(
+                    np.sum((X[rand_idx] - X_rec[rand_idx]) ** 2) / X[rand_idx].shape[0]
+                )
                 self.X_rec_list.append(X_rec[rand_idx])
                 self.X_list.append(X[rand_idx])
-        
+
         return errors_all
-    
+
     def generer_image_DBN(self, nb_data, nb_gibbs):
         for i in range(nb_data):
             v = self.RBM_list[-1].generer_image_RBM_without_plot(nb_gibbs)
 
-            for i in reversed(range(len(self.RBM_list)-1)):
+            for i in reversed(range(len(self.RBM_list) - 1)):
                 v = self.RBM_list[i].sortie_entree_RBM(v)
 
             v = np.reshape(v, (20, 16))
             plt.imshow(v, cmap="gray")
             plt.show()
-            
-            
+
     def display_image_DBN_vs_original(self):
-        
         for i in range(len(self.errors)):
             print(self.epoch_dbn[i])
             fig, ax = plt.subplots(1, 2, figsize=(3, 2))
-            ax[0].imshow(self.X_list[i].reshape(20,16), cmap='gray')
-            ax[0].axis('off')
-            ax[0].set_title('Original_image', fontsize=7)
-            
-            ax[1].imshow(self.X_rec_list[i].reshape(20,16), cmap='gray')
-            ax[1].axis('off')
-            ax[1].set_title(f'RBM_image : RMSE = {self.errors[i]}', fontsize=7)
-            
+            ax[0].imshow(self.X_list[i].reshape(20, 16), cmap="gray")
+            ax[0].axis("off")
+            ax[0].set_title("Original_image", fontsize=7)
+
+            ax[1].imshow(self.X_rec_list[i].reshape(20, 16), cmap="gray")
+            ax[1].axis("off")
+            ax[1].set_title(f"RBM_image : RMSE = {self.errors[i]}", fontsize=7)
+
             fig.tight_layout()
             plt.show()
 
     def generate_for_analysis_DBN(self, nb_gibbs, col=5, row=1):
-        
-        nb_data = row*col
+        nb_data = row * col
 
         fig = plt.figure(figsize=(5, 3))
         grid = plt.GridSpec(row, col, wspace=0.1, hspace=0.1)
@@ -83,18 +82,13 @@ class DBN:
         for i in range(nb_data):
             v = self.RBM_list[-1].generer_image_RBM_without_plot(nb_gibbs)
 
-            for i in reversed(range(len(self.RBM_list)-1)):
+            for i in reversed(range(len(self.RBM_list) - 1)):
                 v = self.RBM_list[i].sortie_entree_RBM(v)
-            
+
             v = np.reshape(v, (20, 16))
             ax = plt.subplot2grid((row, col), (i // col, i % col), fig=fig)
-            ax.imshow(v, cmap='gray')
-            ax.axis('off')
+            ax.imshow(v, cmap="gray")
+            ax.axis("off")
 
         plt.tight_layout()
         plt.show()
-    
-    
-
-
-        
--- principal_DNN_MNIST.py
+++ principal_DNN_MNIST.py
@@ -2,23 +2,31 @@ from principal_DBN_alpha import DBN
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class DNN(DBN):
     def __init__(self, config):
         self.dbn = DBN(config)
         self.classification_layer = []
-        
+
     def pretrain_DNN(self, X, epsilon, batch_size, nb_epochs):
         self.dbn.train_DBN(X, epsilon, batch_size, nb_epochs)
 
     def calcul_softmax(self, rbm, X):
         Z = X @ rbm.RBM_W + rbm.RBM_b
-        e_x = np.exp(Z-np.max(Z), keepdims=True)
+        e_x = np.exp(Z - np.max(Z), keepdims=True)
         return e_x / np.sum(e_x, axis=1, keepdims=True)
-    
+
     def cross_entropy(y_true, y_pred):
         loss = []
         for k in range(y_true.shape[0]):
-            loss.append(np.sum([-y_true[k,j]*np.log(y_pred[k,j]) for j in range(y_true.shape[1])]))
+            loss.append(
+                np.sum(
+                    [
+                        -y_true[k, j] * np.log(y_pred[k, j])
+                        for j in range(y_true.shape[1])
+                    ]
+                )
+            )
         return loss
 
     def entree_sortie_reseau(self, X):
@@ -44,29 +52,35 @@ class DNN(DBN):
                 delta_sortie = sortie_list[-1] - y_batch
 
                 # Update weights
-                last_layer.RBM_W -= epsilon * (sortie_list[-2].T @ delta_sortie) / true_batch_size
-                last_layer.RBM_b -= epsilon * np.mean(delta_sortie, axis=0) / true_batch_size
+                last_layer.RBM_W -= (
+                    epsilon * (sortie_list[-2].T @ delta_sortie) / true_batch_size
+                )
+                last_layer.RBM_b -= (
+                    epsilon * np.mean(delta_sortie, axis=0) / true_batch_size
+                )
 
                 # Update hidden layers
                 for i, rbm in reversed(list(enumerate(self.RBM_list[:-1]))):
-                    delta_sortie = (delta_sortie @ self.RBM_list[i+1].RBM_W.T) * (sortie_list[i+1] * (1 - sortie_list[i+1]))
-                    rbm.RBM_W -= epsilon * (sortie_list[i].T @ delta_sortie) / true_batch_size
-                    rbm.RBM_b -= epsilon * np.mean(delta_sortie, axis=0) / true_batch_size
+                    delta_sortie = (delta_sortie @ self.RBM_list[i + 1].RBM_W.T) * (
+                        sortie_list[i + 1] * (1 - sortie_list[i + 1])
+                    )
+                    rbm.RBM_W -= (
+                        epsilon * (sortie_list[i].T @ delta_sortie) / true_batch_size
+                    )
+                    rbm.RBM_b -= (
+                        epsilon * np.mean(delta_sortie, axis=0) / true_batch_size
+                    )
                 loss_batch += self.cross_entropy(y_batch, sortie_list[-1])
-            
+
             loss.append(np.mean(loss_batch))
-        
+
         return loss
 
     def test_dnn(self, X, y):
         for rbm in self.dbn.RBM_list[:-1]:
             X = rbm.entree_sortie_RBM(X)
         predictions = np.argmax(self.calcul_softmax(self.dbn.RBM_list[-1], X), axis=1)
-        print("Error rate: ", 1 - np.sum(predictions == np.argmax(y, axis=1)) / y.shape[0])
+        print(
+            "Error rate: ", 1 - np.sum(predictions == np.argmax(y, axis=1)) / y.shape[0]
+        )
         return 1 - np.sum(predictions == np.argmax(y, axis=1)) / y.shape[0]
-
-        
-    
-
-
-        
