b100a74cd842a3b0fe5b20f63d120931e63b1386
agatheminaro <agathe.minaro@ensae.fr>
2023-03-18 18:02:11 +0100

    vae comparison

---
+++ principal_VAE_MNIST.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+
+class VAE(torch.nn.Module):
+    def __init__(self, x_dim, h_dim1, h_dim2, z_dim, n_rows, n_cols, n_channels):
+        super(VAE, self).__init__()
+
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.n_channels = n_channels
+        self.n_pixels = (self.n_rows) * (self.n_cols)
+        self.z_dim = z_dim
+
+        # encoder part
+        self.fc1 = torch.nn.Linear(in_features=x_dim, out_features=h_dim1)
+        self.fc2 = torch.nn.Linear(in_features=h_dim1, out_features=h_dim2)
+        self.fc3 = torch.nn.Linear(in_features=h_dim2, out_features=self.z_dim)
+        self.fc32 = torch.nn.Linear(in_features=h_dim2, out_features=self.z_dim)
+        # decoder part
+        self.fc4 = torch.nn.Linear(in_features=z_dim, out_features=h_dim2)
+        self.fc5 = torch.nn.Linear(in_features=h_dim2, out_features=h_dim1)
+        self.fc6 = torch.nn.Linear(in_features=h_dim1, out_features=x_dim)
+
+    def encoder(self, x):
+        h = F.relu(self.fc1(x))
+        h = F.relu(self.fc2(h))
+        return self.fc3(h), self.fc32(h)  # return mu, log_var
+
+    def decoder(self, z):
+        h = F.relu(self.fc4(z))
+        h = F.relu(self.fc5(h))
+        return F.sigmoid(self.fc6(h)).view(
+            -1, self.n_channels, self.n_rows, self.n_cols
+        )
+
+    def sampling(self, mu, log_var):
+        # this function samples a Gaussian distribution, with average (mu) and standard deviation specified (using log_var)
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)  # return z sample
+
+    def forward(self, x):
+        z_mu, z_log_var = self.encoder(x.view(-1, self.n_pixels))
+        z = self.sampling(z_mu, z_log_var)
+        return (
+            self.decoder(z),
+            z_mu,
+            z_log_var,
+        )
+
+    def loss_function(self, x, y, mu, log_var):
+        reconstruction_error = F.binary_cross_entropy(
+            y.view(-1, self.n_pixels), x.view(-1, self.n_pixels), reduction="sum"
+        )
+        KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        return reconstruction_error + KLD
+
+
+def train_vae(vae_model, vae_optimizer, data_train_loader, epoch, verbose=True):
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(data_train_loader):
+        vae_optimizer.zero_grad()
+
+        y, z_mu, z_log_var = vae_model(data)
+        loss_vae = vae_model.loss_function(data, y, z_mu, z_log_var)
+        loss_vae.backward()
+        train_loss += loss_vae.item()
+        vae_optimizer.step()
+
+    if verbose:
+        print(
+            "====> Epoch: {} Average loss: {:.4f}".format(
+                epoch, train_loss / len(data_train_loader.dataset)
+            )
+        )
+
+
+def test_vae(vae_model, data_test_loader, verbose=True):
+    test_loss = 0
+    with torch.no_grad():
+        for _, (data, _) in enumerate(data_test_loader):
+            y, z_mu, z_log_var = vae_model(data)
+            test_loss += vae_model.loss_function(data, y, z_mu, z_log_var).item()
+
+    test_loss /= len(data_test_loader.dataset)
+    if verbose:
+        print("====> Test set loss: {:.4f}".format(test_loss))
+    return test_loss
+
+
+def generate_images(vae_model, n_samples=10):
+    with torch.no_grad():
+        z = torch.randn(n_samples, 1, vae_model.z_dim)
+        samples = vae_model.decoder(z)
+        return samples
+
+
+def pytorch_to_numpy(x):
+    return x.detach().numpy()
+
+
+def display_images(imgs):
+    r = 2
+    c = 5
+    _, axs = plt.subplots(r, c)
+    for j in range(c):
+        # black and white images
+        axs[j].imshow(pytorch_to_numpy(imgs[j, 0, :, :]), cmap="gray")
+        axs[j].axis("off")
+    plt.show()
--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from torch.utils.data import TensorDataset, DataLoader
+import torch
 
 
 def show_alpha_digits(data, row, col):
     indexes = [np.random.randint(0, data.shape[0]) for _ in range(row * col)]
-
     fig, axes = plt.subplots(row, col, figsize=(10, 7))
     for i, idx in enumerate(indexes):
         ax = axes[i // col, i % col]
@@ -18,9 +19,30 @@ def show_alpha_digits(data, row, col):
     plt.show()
 
 
+def show_mnist_digits(X, y, row, col):
+    fig, axes = plt.subplots(row, col, figsize=(8, 5))
+    for i in range(row * col):
+        ax = axes[i // 4, i % 4]
+        ax.imshow(X[i].reshape(28, 28), cmap="gray")
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title(f"Digit {np.argmax(y[i])}")
+
+    fig.subplots_adjust(wspace=0, hspace=0)
+    fig.tight_layout()
+    plt.show()
+
+
 def subsample_data_set(X, y, nb_data=None):
     if nb_data is None:
         return X, y
     else:
         indexes = np.random.permutation(X.shape[0])
         return X[indexes[:nb_data]], y[indexes[:nb_data]]
+
+
+def create_dataloader(X, batch_size=32):
+    X_tensor = torch.Tensor(X)
+    dataset = TensorDataset(X_tensor)
+    dataloader = DataLoader(dataset, batch_size=batch_size)
+    return dataloader
